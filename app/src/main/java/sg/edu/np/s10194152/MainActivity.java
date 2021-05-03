a3f6b5b0d49b0f0cf8d84105f71e695a051d7e65
TJJ101 <S10194152@connect.np.edu.sg>
2021-05-03 13:29:01 +0800

    Just remembered to implement User class into MainActivity. Now fully finished Practical 2

--- app/src/main/java/sg/edu/np/s10194152/MainActivity.java
+++ app/src/main/java/sg/edu/np/s10194152/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        User newUser = new User(false);
         Button followbtn = findViewById(R.id.follow);
         followbtn.setOnClickListener(new View.OnClickListener() {
             @Override
